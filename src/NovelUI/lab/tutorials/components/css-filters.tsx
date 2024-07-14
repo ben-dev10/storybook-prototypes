106028af8f465477166db8715059e738a121e423
ben-dev10 <calvinklien025@gmail.com>
2024-07-14 17:20:32 +0000

    chore: assets added, minor edits

--- src/NovelUI/lab/tutorials/components/css-filters.tsx
+++ src/NovelUI/lab/tutorials/components/css-filters.tsx
@@ -15,11 +15,17 @@ const FILTERS = [
 export default function CSSFilters() {
   return (
     <div className="grid grid-cols-3 gap-3">
+      <div>
+        <img className="nardLogo" src="nard-logo-1.svg" alt="nard logo" />
+      </div>
       {FILTERS.map((classes, index) => (
-        <div
-          key={`${index}`}
-          className={`${classes} filter-box size-20 bg-blue-500`}
-        />
+        <div>
+          <div
+            key={`${index}`}
+            className={`${classes} filter-box size-20 bg-blue-500`}
+          />
+          <small>{classes}</small>
+        </div>
       ))}
     </div>
   );
